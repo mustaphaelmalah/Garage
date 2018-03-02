a9c3208e3f4cd07bf650fd8e254c0c5bcdb096fe
Mustapha Elmalah <mualmalah@gmail.com>
2018-03-02 04:32:54 +0200

    sequi

--- ub-pwd.user.js
+++ ub-pwd.user.js
@@ -7,8 +7,8 @@
 // @match        https://www.theubeg.com:4343/*
 // @include      https://www.theubeg.com:4343/*
 // @grant        none
-// @downloadURL  https://gist.github.com/mustafaelmalah/58c9b7035af944f2ba5e/raw/ub-pwd.user.js
-// @updateURL    https://gist.github.com/mustafaelmalah/58c9b7035af944f2ba5e/raw/ub-pwd.meta.js
+// @downloadURL  https://gist.github.com/mustaphaelmalah/58c9b7035af944f2ba5e/raw/ub-pwd.user.js
+// @updateURL    https://gist.github.com/mustaphaelmalah/58c9b7035af944f2ba5e/raw/ub-pwd.meta.js
 // ==/UserScript==
 
 document.getElementById("testb").readOnly = false;
