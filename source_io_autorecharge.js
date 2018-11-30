86584c1f60904f06d25215436a42bbed5b75f21d
Mustapha Elmalah <mualmalah@gmail.com>
2018-11-30 15:56:08 +0200

    source_io recharge script

---
+++ source_io_autorecharge.js
@@ -0,0 +1,32 @@
+/*
+ * Automatically attempts to recharge firewalls every 1 seconds by using jQuery
+ * to simulate click events.
+ *
+ * Author: Mustapha Elmalah
+ *
+ */
+var recharge_timeout = null;
+function recharge() {
+	if(recharge_timeout) clearTimeout(recharge_timeout);
+	recharge_timeout = setTimeout(function() {
+		// rechange firewall A
+		$("#window-firewall-part1-amount").click(); // open fw page
+		$("#shop-firewall-charge5 .window-shop-element-info").eq(0).click();
+		$("#window-firewall-pagebutton").click(); // go back
+
+		// rechange firewall B
+		$("#window-firewall-part2-amount").click(); // open fw page
+		$("#shop-firewall-charge5 .window-shop-element-info").eq(0).click();
+		$("#window-firewall-pagebutton").click(); // go back
+
+		// rechange firewall C
+		$("#window-firewall-part3-amount").click(); // open fw page
+		$("#shop-firewall-charge5 .window-shop-element-info").eq(0).click();
+		$("#window-firewall-pagebutton").click(); // go back
+
+		// run next cycle
+		recharge();
+	}, 1000);
+}
+
+recharge();
